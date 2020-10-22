419986206a8a5ef5829f466a1aef8ed9b279762b
Fabian Keck <fabian.keck@posteo.de>
2020-10-22 08:23:27 +0200

    put toDo on Paper

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -3,13 +3,26 @@ import styled from 'styled-components';
 import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Paper from "@material-ui/core/Paper";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1),
+            width: theme.spacing(16),
+            height: theme.spacing(16),
+        },
+    },
+}));
 
 export default function Todo({ id, status, description, onAdvance}) {
+    const classes = useStyles();
     return (
-        <StyledTodo>
-            <h3>
-                {description} <small>[{status}]</small>
-            </h3>
+        <Paper elevation={3}>
+            <h3>{description}</h3>
             <ButtonGroup>
                 {status !== 'DONE' && onAdvance && (
                     <Button variant="contained" color="primary"
@@ -20,7 +33,7 @@ export default function Todo({ id, status, description, onAdvance}) {
                 )}
                 <Link to={"/confirmdelete/" + id}><Button variant="contained" color="secondary" >Delete</Button></Link>
             </ButtonGroup>
-        </StyledTodo>
+        </Paper>
     );
 }
 
